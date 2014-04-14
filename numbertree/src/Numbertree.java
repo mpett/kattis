9bee9f30a92207b8710571b926ba14e620ff35b8
Martin Pettersson <martin.pettersson@wemind.se>
2014-04-14 13:33:59 +0200

    Numbertree solution now correct. Also added some cleanup.

--- numbertree/src/Numbertree.java
+++ numbertree/src/Numbertree.java
@@ -1,19 +1,15 @@
-import java.util.ArrayList;
+import java.util.BitSet;
 
-class Node {
-    Node L, R;
-    int label;
-    Node(int label) {
-        this.label = label;
-    }
-}
-
+/**
+ * Solution for Kattis problem-id: numbertree
+ * https://open.kattis.com/problems/numbertree
+ *
+ * Author: Martin Pettersson
+ */
 public class Numbertree {
     Kattio io = new Kattio(System.in, System.out);
-    int height; String path; int rootValue;
-    final static char LEFT = 'L';
-    Node root;
-    ArrayList<ArrayList<Node>> blargh;
+    int height; String path; long rootValue;
+    final static char RIGHT = 'R';
 
     public static void main(String[] args) {
         new Numbertree();
@@ -21,53 +17,48 @@ public class Numbertree {
 
     public Numbertree() {
         handleInput();
-        rootValue = (int) Math.pow(2, (height + 1)) - 1;
-        if(path == null) {
+        // The root value for a perfect binary tree is 2^(H+1)-1.
+        rootValue = (long) Math.pow(2, (height + 1)) - 1;
+        // If input is only the height, then output the root label.
+        if(path == null || path.equals("\"\"") || path.equals(""))
             System.out.println(rootValue);
-        } else {
-            populateTree();
-            System.out.println(findNode());
+        else {
+            // If we interpret the path of left and right as binary numbers,
+            // we can figure out the index of the label on its row in the tree.
+            BitSet bitSet = new BitSet(path.length());
+            int depth = path.length();
+            for(int i = 0; i < depth; i++) {
+                if(path.charAt(depth -i -1) == RIGHT)
+                    bitSet.set(i);
+            }
+            // The row index we are looking for is derived from the path
+            // and the depth of the path.
+            long rowIndex = (long) Math.pow(2, depth) - convert(bitSet) - 1;
+            // We get the row of interest by looking at the depth.
+            // Row = 2 ^ depth. This value is subtracted from the root
+            // label value in order to get the first label value of the row.
+            long subtractor = 0;
+            for(int i = 1; i <= depth; i++)
+                subtractor += (long) Math.pow(2, i);
+            long label = rootValue - subtractor + rowIndex;
+            // Output the found label.
+            System.out.println(label);
+
         }
+        // Flush input.
         io.close();
     }
 
-    void populateTree() {
-        blargh = new ArrayList<ArrayList<Node>>();
-        int count = rootValue;
-        for(int depth = 0; depth <= path.length(); depth++) {
-            int numNodes = (int) Math.pow(2, depth);
-            ArrayList<Node> nodes = new ArrayList<Node>();
-            for(int i = 0; i < numNodes; i++) {
-                Node node = new Node(count);
-                nodes.add(node);
-                count--;
-            }
-            blargh.add(nodes);
+    // Convert a BitSet to an integer (long).
+    long convert(BitSet bits) {
+        long value = 0L;
+        for (int i = 0; i < bits.length(); ++i) {
+            value += bits.get(i) ? (1L << i) : 0L;
         }
-
-        for(int depth = 0; depth < path.length(); depth++) {
-            int lol = 0;
-            for(int i = 0; i < blargh.get(depth).size(); i++) {
-                blargh.get(depth).get(i).L = blargh.get(depth + 1).get(lol);
-                blargh.get(depth).get(i).R = blargh.get(depth + 1).get(lol+1);
-                lol += 2;
-            }
-        }
-
-        root = blargh.get(0).get(0);
+        return value;
     }
 
-    int findNode() {
-        Node tmpNode = root;
-        for(int direction = 0; direction < path.length(); direction++) {
-            if(path.charAt(direction) == LEFT)
-                tmpNode = tmpNode.L;
-            else
-                tmpNode = tmpNode.R;
-        }
-        return tmpNode.label;
-    }
-
+    // Read the input through Kattio.
     void handleInput() {
         height = io.getInt();
         path = io.getWord();
