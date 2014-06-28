252438ebce14c4c371699c56393ddc03923ea1cf
Martin Pettersson <martin.pettersson@wemind.se>
2014-06-28 09:26:24 +0200

    Added comments for id:erase

--- erase/src/Erase.java
+++ erase/src/Erase.java
@@ -1,9 +1,8 @@
 /**
- * Created with IntelliJ IDEA.
- * User: martinpettersson
- * Date: 2014-06-28
- * Time: 08:10
- * To change this template use File | Settings | File Templates.
+ * Solution for Kattis problem-id: erase
+ * https://open.kattis.com/problems/erase
+ *
+ * Author: Martin Pettersson
  */
 public class Erase {
     Kattio io = new Kattio(System.in, System.out);
@@ -16,13 +15,19 @@ public class Erase {
 
     public Erase() {
         handleInput(); String result;
+        // If N is even, the problem is easy. We just need to check that both strings are equal.
         if(N % 2 == 0)
             result = beforeDel.equals(afterDel) ? "Deletion succeeded" : "Deletion failed";
+        // If not, we have to check if the second binary string is the first one reversed.
         else
             result = afterDel.equals(reverseBinary(beforeDel)) ? "Deletion succeeded" : "Deletion failed";
+        // Result printed to stdout
         System.out.println(result);
+        // Flush input
+        io.flush();
     }
 
+    // Takes a binary string as input, and returns the reversed binary string.
     String reverseBinary(String binary) {
         StringBuilder builder = new StringBuilder();
         for(int character = 0; character < binary.length(); character++) {
@@ -33,6 +38,7 @@ public class Erase {
         return builder.toString();
     }
 
+    // Reads from stdin using Kattio according to problem specification.
     void handleInput() {
         N = io.getInt();
         beforeDel = io.getWord();
