05fe46ccb0ea76f326725be2c477bb87154b7e83
Martin Pettersson <martinpettersson@Martins-MacBook-Air.local>
2014-10-17 01:03:14 +0900

    Added solution for id:mixedfractions

---
+++ kattis_java/src/MixedFractions.java
@@ -0,0 +1,34 @@
+/**
+ * Solution for Kattis problem-id: mixedfractions
+ * https://open.kattis.com/problems/mixedfractions
+ *
+ * Author: Martin Pettersson
+ */
+public class MixedFractions {
+    Kattio io = new Kattio(System.in, System.out);
+    private int numerator, denominator;
+
+    public static void main(String[] args) {
+        new MixedFractions();
+    }
+    
+    public MixedFractions() {
+        boolean hasMoreTestCases = handleInput();
+        int numInteger, remainder = 0;
+        while (hasMoreTestCases) {
+            remainder = numerator % denominator;
+            numInteger = (numerator - remainder) / denominator;
+            System.out.println(numInteger + " " + remainder + " / " + denominator);
+            hasMoreTestCases = handleInput();
+        }
+        io.flush();
+    }
+
+    boolean handleInput() {
+        numerator = io.getInt();
+        denominator = io.getInt();
+        if (numerator == 0 && denominator == 0)
+            return false;
+        return true;
+    }
+}
