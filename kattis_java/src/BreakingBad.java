720eab65872f8f0f060f11ffc00f6af2c16e1900
Martin Pettersson <kontakt@martinpettersson.se>
2015-03-02 17:37:34 +0900

    Input handling now working correctly for id:breakingbad

--- kattis_java/src/BreakingBad.java
+++ kattis_java/src/BreakingBad.java
@@ -1,11 +1,11 @@
-
+import java.util.HashMap;
 
 /**
  * Created by martinpettersson on 27/02/15.
  */
 public class BreakingBad {
     Kattio io = new Kattio(System.in);
-    private int N, M;
+    private int N, M; private String[] items; private HashMap<String, Integer> itemIndices;
 
     public static void main(String[] args) {
         new BreakingBad();
@@ -19,25 +19,22 @@ public class BreakingBad {
 
     AdjMatrixGraph handleInput() {
         N = io.getInt();
-        String[] items = new String[N];
-        for (int item = 0; item < N; item++)
-            items[item] = io.getWord();
+        items = new String[N];
+        itemIndices = new HashMap<String, Integer>();
+        String currentItem;
+        for (int item = 0; item < N; item++) {
+            currentItem = io.getWord();
+            items[item] = currentItem;
+            itemIndices.put(currentItem, item);
+        }
         M = io.getInt();
-        AdjMatrixGraph itemGraph = new AdjMatrixGraph(hashItem(io.getWord()), hashItem(io.getWord()));
-        for (int dangerousPair = 1; dangerousPair < M; dangerousPair++) {
+        AdjMatrixGraph itemGraph = new AdjMatrixGraph(N);
+        System.err.println(M);
+        for (int i = 0; i < M; i++) {
             String firstItem = io.getWord(); String secondItem = io.getWord();
-            int V = hashItem(firstItem); int E = hashItem(secondItem);
-            itemGraph = new AdjMatrixGraph(V, E);
+            int V = itemIndices.get(firstItem); int E = itemIndices.get(secondItem);
+            itemGraph.addEdge(V, E);
         }
         return itemGraph;
-    }
-
-    int hashItem(String item) {
-        int hash=3;
-        int strlen = item.length();
-        for (int i=0; i < strlen; i++) {
-            hash = hash+item.charAt(i);
-        }
-        return Math.abs(hash);
     }
 }
