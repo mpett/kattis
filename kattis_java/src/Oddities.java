3206ab260594d94d25f23d69d7430e8a334a5fcc
Martin Pettersson <kontakt@martinpettersson.se>
2016-02-03 03:39:42 +0100

    Oddities solution

---
+++ kattis_java/src/Oddities.java
@@ -0,0 +1,18 @@
+/**
+ * Created by martinpettersson on 28/01/16.
+ */
+public class Oddities {
+    private static Kattio io = new Kattio(System.in);
+
+    public static void main(String[] args) {
+        int numTestCases = io.getInt();
+        for (int testCase = 0; testCase < numTestCases; testCase++) {
+            int number = io.getInt();
+            if (number % 2 == 0)
+                System.out.println(number + " is even");
+            else
+                System.out.println(number + " is odd");
+        }
+
+    }
+}
